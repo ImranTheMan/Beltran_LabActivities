6bd7351e3e52ee38d91d2035085309305869c8ea
IMRAN JASON BELTRAN <142290383+ImranTheMan@users.noreply.github.com>
2024-04-08 11:33:38 +0000

    update

--- todolist/todolist/src/Item.js
+++ todolist/todolist/src/Item.js
@@ -1,44 +1,46 @@
 // Item.js
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./Button.css";
 
 const Item = ({ item, onDeleteClick, onToggleCheck, onEdit }) => {
-    const [newName, setNewName] = useState("");
+  const [newName, setNewName] = useState("");
 
-    const handleButtonClick = () => {
-        onToggleCheck(item.id); // Notify the parent component about the toggle
-    };
+  const handleButtonClick = () => {
+    onToggleCheck(item.id); // Notify the parent component about the toggle
+  };
 
-    const handleDeleteClick = () => {
-        onDeleteClick(item.id);
-    };
+  const handleDeleteClick = () => {
+    onDeleteClick(item.id);
+  };
 
-    const handleEditClick = () => {
-        const newName = prompt("Enter the new name:", item.input);
-        if (newName !== null) {
-            onEdit(item.id, newName);
-        }
-    };
+  const handleEditClick = () => {
+    const newName = prompt("Enter the new name:", item.input);
+    if (newName !== null) {
+      onEdit(item.id, newName);
+    }
+  };
 
-    return (
-        <div className="todo-list">
-            <div className="todo-item" style={{ textDecoration: item.isChecked ? 'line-through' : 'none' }}>
-                <span className="quantity">{item.quantity}</span>
-                <span className="input">{item.input}</span>
-                
-                <button className="button-54" onClick={handleButtonClick}>
-                    Done
-                </button>
-                <button className="button-54" onClick={handleDeleteClick}>
-                    Delete
-                </button>
-                <button className="button-54" onClick={handleEditClick}>
-                    Edit
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="todo-list">
+      <div
+        className="todo-item"
+        style={{ textDecoration: item.isChecked ? "line-through" : "none" }}
+      >
+        <span className="quantity">{item.quantity}</span>&nbsp;&nbsp;&nbsp;
+        <span className="input">{item.input}</span>&nbsp;
+        <button className="button-54" onClick={handleButtonClick}>
+          Done
+        </button>
+        <button className="button-54" onClick={handleDeleteClick}>
+          Delete
+        </button>
+        <button className="button-54" onClick={handleEditClick}>
+          Edit
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Item;
